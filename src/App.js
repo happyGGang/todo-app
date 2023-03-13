1f9f535a1390b0e6ffd7cd2d9451a05a0f3eae3e
ykkang <ykkang@ocube.co.kr>
2023-03-13 14:56:14 +0900

    [style] ListItem 스타일링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
